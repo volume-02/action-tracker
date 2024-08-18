9e04eb7ee5c1c6c9775a641883864d8dc5d8c04b
volume02 <borsukboris@gmail.com>
2024-08-18 18:20:21 +0700

    update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,20 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
     { files: ['**/*.{js,mjs,cjs,ts}'] },
-    { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    {
+        rules: {
+            'no-unused-vars': 'off',
+            'no-undef': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/triple-slash-reference': 'off',
+        },
+    },
     eslintConfigPrettier,
-]
+];
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/* eslint @typescript-eslint/no-require-imports: 0 */ //can't use import here
+
+const webpack = require('webpack');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((acc, val) => {
+    acc[`process.env.${val}`] = JSON.stringify(env[val]);
+    return acc;
+}, {});
+
+module.exports = {
+    entry: './src/tracker/tracker.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'tracker.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'Tracker',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+    },
+    plugins: [new webpack.DefinePlugin(envKeys)],
+    mode: 'production',
+    watch: true,
+};
